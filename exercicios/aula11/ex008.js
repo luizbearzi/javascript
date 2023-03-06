947785ab001db1006e5b06d4e67b700b3ff48e1e
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-03-06 13:09:49 -0300

    atualizado aula 11

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 40.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. Você foi MULTADO')
+} else {
+    console.log('Parabéns continue dirigindo dentro do limite de velocidade!')
+}
+console.log('Dirija sempre usando cinto de segurança')
