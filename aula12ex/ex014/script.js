78ec06e5d7553d5889ab243981185673d9a471a4
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-03-08 00:07:17 -0300

    atualizado exercicios parte 2

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 21
+    
+    msg.innerHTML= `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/fotomanha.jpg'
+        document.body.style.background = '#e6d3c4'
+        // BOM DIA
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/fototarde.jpg'
+        document.body.style.background = '#d7936e'
+        // BOA TARDE
+    } else {
+        img.src = 'imagens/fotonoite.jpg'
+        document.body.style.background ='#195d8a'
+        // BOA NOITE
+    }
+}
+
