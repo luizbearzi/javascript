2228be26543c3d8aea1ba73c8f2ab4016c8e1c77
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-03-14 13:03:08 -0300

    atualizando aula 14

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`Contando... ${c}`)
+    c++
+}
+*/
+
+// --- Código em For
+console.log('Vai começar...')
+for (var c = 1; c <= 5; c++) {
+    console.log(`Contando com for..... ${c}`)
+}
+console.log('FIM')
